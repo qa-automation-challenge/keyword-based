4e1da2b24012cb2bf8fda03d2f4dbe5e45613b35
Rui Braz Rodrigues <rui.braz.rodrigues@celfocus.com>
2021-03-29 18:05:48 +0100

    Add: cucumber steps

---
+++ src/main/java/browserTesting/Steps.java
@@ -0,0 +1,62 @@
+package browserTesting;
+
+import java.net.URL;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Steps {
+
+    WebDriver driver;
+    String baseURL, nodeURL;
+
+    @Given("^I open Chrome and launch the application$")
+    public void openFirefoxAndLaunchApplication() throws Throwable
+    {
+        baseURL = "https://qa-automation-challenge.github.io/sandbox/";
+        nodeURL = "http://localhost:4444/wd/hub";
+        ChromeOptions expectedChromeOptions = new ChromeOptions();
+        driver = new RemoteWebDriver(new URL(nodeURL), expectedChromeOptions);
+        driver.get(baseURL);
+    }
+
+    @When("^I select type \"(.*)\"$")
+    public void selectType(String type)
+    {
+        Select select = new Select(driver.findElement(By.id("type")));
+        select.selectByVisibleText(type);
+    }
+
+    @When("^I select support plan \"(.*)\"$")
+    public void selectSupportPlan(String plan)
+    {
+        Select select = new Select(driver.findElement(By.id("support")));
+        select.selectByVisibleText(plan);
+    }
+
+    @When("^I write monthly duration of \"(.*)\"$")
+    public void writeMonthlyDuration (String duration)
+    {
+        driver.findElement(By.id("duration")).sendKeys(duration);
+    }
+
+    @When("^I click in calculate price button")
+    public void clickCalculatePriceButton()
+    {
+        driver.findElement(By.id("calculate")).click();
+    }
+
+    @Then("^I validate price is \"(.*)\"$")
+    public void	validatePrice(String price) throws InterruptedException {
+        Thread.sleep(5*1000);
+        Assert.assertEquals(price, driver.findElement(By.id("price")).getText());
+    }
+
+}
